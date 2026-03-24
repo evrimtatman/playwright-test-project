ee7db1642e3121a85f10b0aa803aaff36726d9f5
Evrim Cakan <evrim.cakan@avenga.com>
2026-03-24 11:13:23 +0100

    Baseurl update

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-	baseURL: process.env.BASE_URL || 'https://fakerestapi.azurewebsites.net',
+	  baseURL: process.env.BASE_URL ?? 'https://fakerestapi.azurewebsites.net',
 
     extraHTTPHeaders: {
       'Content-Type': 'application/json',
